759479acd68dfea10473974f5d5b1045aab07c75
Inspecode <inspecode-bot@rocro.com>
2018-01-10 05:19:45 +0000

    Fix Prettier errors

--- docs/assets/js/init.js
+++ docs/assets/js/init.js
@@ -1,1 +1,6 @@
-window.Herman=function(i,n){"use strict";return n(function(){window.hljs.initHighlightingOnLoad(),i.initializeToggles(),i.initializeTabs(),i.initializeIframes()}),i}(window.Herman||{},window.jQuery);
+window.Herman = (function(i, n) {
+  "use strict";
+  return n(function() {
+    window.hljs.initHighlightingOnLoad(), i.initializeToggles(), i.initializeTabs(), i.initializeIframes();
+  }), i;
+})(window.Herman || {}, window.jQuery);
